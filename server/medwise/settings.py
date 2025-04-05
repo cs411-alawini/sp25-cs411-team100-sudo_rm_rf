16c8f0c91303201853265a9ead5b1df550ce9d79
Daniel S <44075071+Unkwn1@users.noreply.github.com>
2025-04-05 18:45:47 -0500

    working on db connection

--- server/medwise/settings.py
+++ server/medwise/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'medwise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cs411',
+        'USER': 'djangouser',
+        'PASSWORD': 'strong_password',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
