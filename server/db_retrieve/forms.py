621bb96453443eae5ea44907d9fd763204634901
Daniel S <44075071+Unkwn1@users.noreply.github.com>
2025-04-07 20:57:52 -0500

    Added search box to db retrieval

--- server/db_retrieve/forms.py
+++ server/db_retrieve/forms.py
@@ -4,10 +4,3 @@ from django.core.exceptions import ValidationError
 
 class getRxaui(forms.Form):
     requested_id = forms.IntegerField(help_text="Enter a valid RXAUI integer id.")
-
-    def clean_input_id(self):
-        data = self.requested_id
-        
-        # perform some data validation checks
-
-        return data
--- server/db_retrieve/views.py
+++ server/db_retrieve/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
 from django.urls import reverse
-from django.views import generic
-from django.template import loader
+#from django.views import generic
+#from django.template import loader
+from django.views.generic.list import ListView
+from django.db.models import Q
 
 from .models import Rxnconso
 from .forms import getRxaui
@@ -10,16 +12,32 @@ from .forms import getRxaui
 def index(request):
     return HttpResponse("testing")
 
+
 def searchbox(request):
-    template = loader.get_template("db_retrieve/search.html")
-    return HttpResponse(template.render(request))
+    query = request.GET.get("drug_id_input")
+    #return redirect("result/", aui=query)
+    #return redirect(result, aui = query)
+    return render(request, "db_retrieve/search.html")
 
-def result(request, aui):
-    id = get_object_or_404(Rxnconso, pk = aui)
-    return render(request, "db_retrieve/results.html", {"str" : id})
+def search(request):
+    return render(request, "db_retrieve/search.html")
+    #vectors = Rxnconso.objects.all()
+    #result = Rxnconso.objects.filter(Q(rxaui__icontains = query))
+    #return result(request, query)
 
+def result(request):
+    query = request.GET.get("searched_aui")
+    result = Rxnconso.objects.get(rxaui = str(query))
+    return render(request, "db_retrieve/results.html", {"result" : result})
+    """
+    else:
+        id = get_object_or_404(Rxnconso, pk = aui)
+        return render(request, "db_retrieve/results.html", {"str" : id})
+    """
+
+"""
 def searchresult(request, aui):
-    drug_instance = get_object_or_404(Rxconsno, pk = aui)
+    drug_instance = get_object_or_404(Rxnconso, pk = aui)
 
     return render(request, "db_retrieve/results.html", {"Drug name": rxnconso})
-
+"""
