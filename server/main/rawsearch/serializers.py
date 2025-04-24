6678862409efc7edec4ca3433d8afc65e4590243
Unkwn1 <daniel.shevchuk1@gmail.com>
2025-04-24 13:18:54 -0500

    changed to use sql cursor

--- server/main/rawsearch/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models import Rxnconso
-
-class SearchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Rxnconso
-        fields = "__all__"
--- server/main/rawsearch/views.py
+++ server/main/rawsearch/views.py
@@ -1,11 +1,17 @@
 from ..models import Rxnconso
-from .serializers import SearchSerializer
+from django.db import connection
 from rest_framework.views import APIView
 from rest_framework.response import Response
     
 class SearchItemView(APIView):
     def get(self, request):
         query = request.GET.get('q', '')
-        items = Rxnconso.objects.filter(rxcui=query)
-        serializer = SearchSerializer(items, many=True)
-        return Response(serializer.data)
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT str, rxaui, sab, rxcui FROM rxnconso WHERE rxcui LIKE %s",
+            [query]
+        )
+        
+        # Convert returned rows into a dictionary
+        columns = [col[0] for col in cursor.description]
+        return Response([dict(zip(columns, row)) for row in cursor.fetchall()])
